31fe2f85898ba5165551c72e0cba77a848ef8c0f
JadoGarBhai <sikandernawaz35@gmail.com>
2023-06-28 17:29:08 +0500

    Methods

--- 28-June-2023/array.js
+++ 28-June-2023/array.js
@@ -10,7 +10,7 @@
 
 // console.log(arr[2]);     To get specific value.
 
-//practice Set.
+                                //PRACTICE SET.
 // 1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
 // 2. Check your list length in the console.
 // 3. Update "Bread" to "Bananas."
@@ -24,7 +24,7 @@
 //\\//\\//\\///\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\//\\//\\//\\//\\//\\
 
 // Now we discuss and practice of methods.
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
                     
 // arr.push("Push");    // Add value at the end of array.
 // console.log(arr);
@@ -55,4 +55,60 @@ let arr = [1, 2, 3, 4, 5];
 
 // let arr1 = arr.lastIndexOf(5);       // output the last index number of the array.
 // console.log(arr1);
+
+// let arr1 = [9, 6, 8, 10, 7, 11];
+// let array = arr1.reverse();
+// console.log(array);
+
+// let array1 = arr1.sort((a, b) => a - b);     // Sorting numeric values in accending order.
+// console.log(array1);
+
+// let arr3 = arr.concat(arr1);     // Join the two arrays.
+// console.log(arr3);
+
+// arr3.sort((a, b) => b - a);     // Sorting numeric values in deccending order.
+// console.log(arr3);
+
+                                //PRACTICE SET.
+// 1. Create an empty array to use as a shopping list.
+// 2. Add Milk , Bread , and Apples to your list.
+// 3. Update " Bread " with Bananas and Eggs .
+// 4. Remove the last item from the array and output it into the console.
+// 5. Sort the list alphabetically.
+// 6. Find and output the index value of Milk .
+// 7. After Bananas , add Carrots and Lettuce .
+// 8. Create a new list containing Juice and Pop .
+// 9. Combine both lists, adding the new list twice to the end of the first list.
+// 10. Get the last index value of Pop and output it to the console.
+
+// let shoppingList = [];
+// shoppingList.splice(0, 0, "Milk", "Bread", "Apples");   // output: [ 'Milk', 'Bread', 'Apples' ].
+// // console.log(shoppingList);
+
+// shoppingList.splice(1, 1, "Bananas", "Eggs");           // output: [ 'Milk', 'Bananas', 'Eggs', 'Apples' ].
+// // console.log(shoppingList);
+
+// shoppingList.pop();                                     // output: [ 'Milk', 'Bananas', 'Eggs' ].
+// // console.log(shoppingList);
+
+// shoppingList.sort();                                    // output: [ 'Bananas', 'Eggs', 'Milk' ].
+// // console.log(shoppingList);
+
+// let sL = shoppingList.findIndex(item => item === "Milk");
+// // console.log(sL);
+
+// shoppingList.splice(1, 0, "Carrots", "Lettuce");      // output: [ 'Bananas', 'Carrots', 'Lettuce', 'Eggs', 'Milk' ].
+// // console.log(shoppingList);
+
+// let shoppingList2 = ["Juice", "Pop"];
+// let list = shoppingList.concat(shoppingList2);
+// // console.log(list);
+
+// let finalList = shoppingList.concat(list).concat(list);
+// // console.log(finalList);
+
+// let index = finalList.lastIndexOf("Pop");
+// console.log(index);
+
+//\\//\\//\\///\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\//\\//\\//\\//\\//\\
 
