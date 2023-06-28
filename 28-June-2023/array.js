5765f10a57cccd17424ffdde41a7e1d63adf5356
JadoGarBhai <sikandernawaz35@gmail.com>
2023-06-28 16:32:00 +0500

    array

---
+++ 28-June-2023/array.js
@@ -0,0 +1,58 @@
+// Here, we learn how to make array and methods of array.
+
+// let arr = [1, "Ali", true];
+// console.log(arr);
+
+// over-writting the array.
+// arr[0] = "RG";
+// arr[1] = 6;
+// console.log(arr);
+
+// console.log(arr[2]);     To get specific value.
+
+//practice Set.
+// 1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
+// 2. Check your list length in the console.
+// 3. Update "Bread" to "Bananas."
+// 4. Output your entire list to the console.
+
+// let shoppingList = ["Milk", "Bread", "Apples"];
+// console.log(shoppingList.length);
+// shoppingList[1] = "Bananas";
+// console.log(shoppingList);
+
+//\\//\\//\\///\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\//\\//\\//\\//\\//\\
+
+// Now we discuss and practice of methods.
+let arr = [1, 2, 3, 4, 5];
+                    
+// arr.push("Push");    // Add value at the end of array.
+// console.log(arr);
+
+// arr.unshift(0);          // Add value from the start of array.
+// console.log(arr);
+
+// arr.splice(3, 0, "Add at", "Specific place");  // Add the value at specific place.
+// console.log(arr);
+
+// arr.splice(3, 2, "delete", "values");
+// console.log(arr);
+
+// arr.pop();                     // Remove value at the end of Array.
+// console.log(arr);
+
+// arr.shift();                     // Remove value from the start of array.
+// console.log(arr);
+
+// arr.splice(1, 3);                    // Delete value from the specific place.
+// console.log(arr);
+
+// let arr1 = arr.find(item => item > 2);  // Find value on the given index.
+// console.log(arr1);
+
+// let arr1 = arr.indexOf(3);               // In this, we give the value and "indexOf()" answer me the index number of array. 
+// console.log(arr1);
+
+// let arr1 = arr.lastIndexOf(5);       // output the last index number of the array.
+// console.log(arr1);
+
